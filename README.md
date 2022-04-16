34bf51d4e1a01adf49e7f66f39519ea4aa81823f
Apolinar Fernandez <afcpolo1983@gmail.com>
2022-04-16 00:38:21 -0500

    primer ejercicio practico de javascript area del cuadrado

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # platzi-curso-practico-javascript
+
+...
+
+## Taller #1: Figuras Geométricas
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
+  
+
+
+
---
+++ figuras.js
@@ -0,0 +1,11 @@
+// Código del Cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: "  + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
+
+// Código del Triangulo
