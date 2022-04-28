445b575434b60f2a5551009db0a890793bb6d0f8
Apolinar Fernandez <afcpolo1983@gmail.com>
2022-04-27 22:30:13 -0500

    se elimina el archivo figuras de javascript y se crea nuevamente el archivo html

--- figuras.js
+++
@@ -1,7 +0,0 @@
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado);
-
-const  perimetroCuadrado =ladoCuadrado * 4;
-console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
